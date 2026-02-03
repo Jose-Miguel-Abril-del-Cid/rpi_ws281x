7d5106d04d8b1c86ec7753385348d87409260400
Jose Abril <jmabril@nisa.com.gt>
2026-02-03 11:23:18 -0600

    Update LED_COUNT to match the actual number of LEDs (8 -> 11)

--- setcolor.c
+++ setcolor.c
@@ -6,7 +6,7 @@
 #include "ws2811.h"
 
 // ====== AJUSTA ESTO ======
-#define LED_COUNT   8          // <-- tu cantidad real de LEDs
+#define LED_COUNT   11          // <-- tu cantidad real de LEDs
 #define LED_GPIO    18         // GPIO 18 (PWM)
 #define LED_DMA     10
 #define LED_INVERT  0
